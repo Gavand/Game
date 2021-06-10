4ae659338fb3f34c6526b099cedd76442d2978a1
Gavin <>
2021-06-10 12:18:57 -0700

    Simple Gravity Game
    UIDynamicBehavior - Recognizes accelerometer

---
+++ Game/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Game
+//
+//  Created by Gavin Andrews on 3/19/20.
+//  Copyright © 2020 Gavin Andrews. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
